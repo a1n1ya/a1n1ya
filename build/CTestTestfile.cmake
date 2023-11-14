ac4dbc67d26fd4e00832ac523b7078063e084594
an1ya1 <1486476138@qq.com>
2023-11-14 22:09:16 +0800

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/shenyexiang/yolo_v5/src
+# Build directory: /home/shenyexiang/yolo_v5/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("detection_msgs")
+subdirs("yolov5_ros")
---
+++ build/detection_msgs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shenyexiang/yolo_v5/src/detection_msgs
+# Build directory: /home/shenyexiang/yolo_v5/build/detection_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
